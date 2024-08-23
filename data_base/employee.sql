0ac554cb358fc5f932be2b5994784e20608e3bfe
srujana200515 <hm.sruj@gmail.com>
2024-08-23 17:09:24 +0530

    persons and employees added

---
+++ data_base/employee.sql
@@ -0,0 +1,34 @@
+use srujana_db;
+create table employees(id int primary key auto_increment, name varchar(50) not null, designation varchar(40) not null, technology varchar(30) not null, phone_num bigint unique, commission int, salary float default 0, years_of_exp int);
+select * from persons;
+ insert into employees(name,designation,technology,phone_num,commission, salary,years_of_exp) values('srujana','manager','python','2222222222','40','100000','5');
+ 
+ insert into employees(name,designation,technology,phone_num,commission,salary,years_of_exp) values('sinchana','manager','java','100066000','50','1000000','6');
+
+insert into employees(name,designation,technology,phone_num,commission,salary,years_of_exp) values('Neeha','teamleader','c','00258413320','10','100000','5');
+
+insert into employees(name,designation,technology,phone_num,commission,salary,years_of_exp) values('bhagya','assistent','c++','553322110','10','100000','5');
+
+
+insert into employees(name,designation,technology,salary,years_of_exp) values('mala','manager','python','100000','5');
+
+
+insert into employees(name,designation,technology,phone_num,commission,salary,years_of_exp) values('vijji','employee','c','5841229035','10','100000','5');
+
+insert into employees(name,designation,technology,phone_num,commission,salary,years_of_exp) values('yukta','manager','python','1029384756','10','100000','5');
+
+insert into employees(name,designation,technology,phone_num,commission,salary,years_of_exp) values('pallavi','teamleader','rust','713245890','10','100000','5');
+
+insert into employees(name,designation,technology,phone_num,commission,salary,years_of_exp) values('disha','manager','javascript','789012345','10','100000','5');
+
+insert into employees(name,designation,technology,phone_num,commission,salary,years_of_exp) values('pavitra','teamleader','java','984454210','10','100000','5');
+
+
+
+insert into employees(name,designation,technology,phone_num,salary,years_of_exp) values('nandini','designer','python','0512006660','3500000','5');
+
+insert into employees(name,designation,technology) values('taniea','designer','python');
+select * from employees;
+
+
+
---
+++ data_base/person.sql
@@ -0,0 +1,76 @@
+create table IF NOT EXISTS persons(id int primary key auto_increment , name varchar(50) not null, gender varchar(2), location varchar(50),age int check(age > 0));
+
+ select * from persons;
+ 
+ insert into persons(name,gender,location,age) values('srujana' ,'F' ,'Raichur',19);
+ 
+ insert into persons(name,gender,location,age) values('sinchana' ,'F' ,'Raichur',20);
+ 
+ insert into persons(name,gender,location,age) values('Neeha' ,'F' ,'Raichur',19);
+ 
+ insert into persons(name,gender,location,age) values('Bhagya' ,'F' ,'Raichur',19);
+ 
+ insert into persons(name,gender,location,age) values('Yukta' ,'F' ,'Bijapur',19);
+ 
+ insert into persons(name,gender,location)    values('srujana' ,'F' ,'Raichur');
+ 
+ insert into persons(name,gender,location,age) values('Pallavi' ,'F' ,'hospet',19);
+ 
+ 
+ insert into persons(name,gender,location)    values('Disha' ,'F' ,'bagalkot');
+ 
+ insert into persons(name,gender,location)    values('sru' ,'F' ,'Raichur');
+ 
+ insert into persons(name,gender,age)          values ('srujana' ,'F' ,19);
+ 
+ insert into persons(name,gender)              values('srujana' ,'F' );
+ 
+ insert into persons(name,gender,location)    values('srujana' ,'F' ,'Raichur');
+ 
+ insert into persons(name,gender,location,age) values('srujana' ,'F' ,'lingusugur',19);
+ 
+ insert into persons(name,gender,location,age) values('srujana' ,'F' ,'Raichur',19);
+ 
+ insert into persons(name,gender,location,age) values('srujana' ,'F' ,'Raichur',19);
+ 
+ insert into persons(name,gender,location,age) values('srujana' ,'F' ,'Raichur',19);
+ 
+ insert into persons(name,gender,location,age) values('srujana' ,'F' ,'Raichur',19);
+ 
+ insert into persons(name)                     values('srujana' );
+ 
+ insert into persons(name,gender,location)    values('Akash' ,'m' ,'Bijapur');
+ 
+ insert into persons(name,gender,location,age) values('Yukta' ,'f' ,'Bijapur',19);
+ 
+ insert into persons(name,gender,location,age) values('Nandini' ,'f' ,'belgavi',20);
+ 
+ insert into persons(name,gender,location,age) values('Nandini' ,'f' ,'belgavi',20);
+ 
+ insert into persons(name,gender,location,age) values('Taniea' ,'f' ,'Honnavar',19);
+ select name,location from persons;
+ 
+ select location from persons;
+ select distinct location from persons;
+ select * from persons where age < 50;
+ select * from persons  where location='Raichur';
+ select * from persons  where age between 15 and 20;
+ select * from persons where name like 'P%';
+ select * from persons where name like 'P___a';
+ select * from persons  where location in ('bijapur','belgavi','bagalkot');
+ select * from persons  where location ='Raichur' or location='belgavi';
+ select * from persons  where location =null;
+ select * from persons  where location ='null';
+ select * from persons  where location is null;
+ 
+ 
+ 
+ 
+ 
+
+
+
+ 
+ 
+ 
+ 
