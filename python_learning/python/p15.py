f702568eee80f397a29e494f1a6c49752de0da9a
srujana200515 <hm.sruj@gmail.com>
2024-08-24 16:32:40 +0530

    addition of p15

---
+++ python_learning/python/p15.py
@@ -0,0 +1,10 @@
+#Program to find Sum of even(odd) digits in a number
+number=int(input("Enter the  range numbers to find the sum of even(odd) :"))
+even_sum=0
+odd_sum=0
+for i in range (0,number,2):
+        even_sum+=i
+print("The sum of even number",even_sum)
+for i in range (1,number,2):
+        odd_sum+=i
+print("The sum of odd number",odd_sum)
